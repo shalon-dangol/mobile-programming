9a5983c28df1a7b5bd8058b7487ad9b12102b431
Shalon Dangol <shalondangol.sd@gmail.com>
2023-04-18 00:08:05 +0545

    updated

--- app/src/main/java/com/example/test/LoginActivity.java
+++ app/src/main/java/com/example/test/LoginActivity.java
@@ -3,12 +3,13 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText edtEmail,edtPassword;
     private AppCompatButton btnLogin;
     private TextView txtForgotPassword, txtSignup, txtWelcome;
@@ -26,13 +27,29 @@ public class LoginActivity extends AppCompatActivity {
         txtWelcome = findViewById(R.id.txtWelcome);
         txtForgotPassword = findViewById(R.id.txtForgotPassword);
         txtSignup = findViewById(R.id.txtSignup);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(edtEmail.getText().toString().isEmpty()&&edtPassword.getText().toString().isEmpty()){
+        btnLogin.setOnClickListener(this);
+        txtSignup.setOnClickListener(this);
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(edtEmail.getText().toString().isEmpty()&&edtPassword.getText().toString().isEmpty()){
+//
+//
+//                }
+//            }
+//        });
+    }
 
-                }
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btnLogin){
+
+        }
+        else if(view.getId()==R.id.txtSignup){
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
     }
 }
